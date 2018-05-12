78afc96aa301c06f407841ae60c17fcecc62329e
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2018-05-12 09:54:04 +0700

    testing

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv').config()
 const Action = require('./service/action')
 const Indodax = require('./service/indodax')
 
-Indodax.action.tradeHistory(['doge', 'btc']).then(res => {
+Indodax.action.openOrders(['doge', 'btc']).then(res => {
     console.log(res)
 }).catch(console.error)
