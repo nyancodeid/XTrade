4039fc869df62ec11577d6bebe6e8fb67cd37f01
Ryan Aunur Rassyid <ryandevstudio@gmail.com>
2018-05-12 09:53:54 +0700

    adding buy, sell, openOrders exec

--- service/indodax.js
+++ service/indodax.js
@@ -3,14 +3,30 @@ const uriQuery = require('http-build-query')
 const hmacSHA512 = require('crypto-js/hmac-sha512')
 const axios = require('axios')
 
+const ErrorProvider = require('./errors')
+
 let Indodax = {
     toPair(objectPair) {
         if (_.isArray(objectPair)) {
             return objectPair.join('_')
+        } else {
+            throw new Error(ErrorProvider.ERRPAIR.message);
         }
     },
     toTicker(pair) {
         return process.env.URI_API + pair + "/ticker"
+    },
+    isPairIDR(pair) {
+        if (_.isArray(pair)) {
+
+            return (pair[1].toLowerCase() === "btc") ? true : false
+        }
+    },
+    isPairBTC(pair) {
+        if (_.isArray(pair)) {
+
+            return (pair[1].toLowerCase() === "idr") ? true : false
+        }
     },
     makeSign(datas) {
         if (_.isObject(datas)) {
@@ -38,18 +54,10 @@ let Action = function() {
 }
 
 /**
- * @description get ticker for pair
+ * @description get account balance every currency
+ * 
+ * @returns { Object }
  */
-Action.prototype.get = function (pair) {
-    pair = Indodax.toPair(pair)
-
-    return axios.get(Indodax.toTicker(pair)).then(body => {
-        if (body.status === 200) {
-            
-            return body.data.ticker
-        }
-    })
-}
 Action.prototype.balance = function() {
 
     return Indodax.execRequest({
@@ -63,6 +71,13 @@ Action.prototype.balance = function() {
         }
     })
 }
+
+/**
+ * @description get trading history by Pair
+ * @param pair { Array }
+ * 
+ * @returns { Object }
+ */
 Action.prototype.tradeHistory = function(pair) {
     pair = Indodax.toPair(pair)
 
@@ -78,11 +93,118 @@ Action.prototype.tradeHistory = function(pair) {
         }
     })
 }
-Action.prototype.buy = function() {
-    
-}
-Action.prototype.sell = function () {
+/**
+ * @description get trading open orders if with pair is return Array
+ * @param pair { Array }
+ * 
+ * @returns { Object }
+ */
+Action.prototype.openOrders = function(pair) {
+    let req = {
+        method: 'openOrders'
+    }
 
+    if (!_.isUndefined(pair)) {
+        req.pair = Indodax.toPair(pair)
+    }
+    
+    return Indodax.execRequest(req).then(res => {
+        if (res.status === 200) {
+            if (res.data.success) {
+
+                return res.data.return.orders
+            } 
+        }
+    })
+}
+
+
+/**
+ * @description get ticker for pair
+ * @param pair { Array }
+ * 
+ * @returns { Object }
+ */
+Action.prototype.get = function (pair) {
+    pair = Indodax.toPair(pair)
+
+    return axios.get(Indodax.toTicker(pair)).then(body => {
+        if (body.status === 200) {
+
+            return body.data.ticker
+        }
+    })
+}
+
+/**
+ * @description do buy trading
+ * @param config { Object }
+ * 
+ * @returns { Object }
+ */
+Action.prototype.buy = function(config) {
+    let pair = Indodax.toPair(config.pair)
+    let request = {
+        method: 'trade',
+        pair: pair,
+        type: 'buy',
+        price: config.price
+    }
+
+    if (Indodax.isPairBTC(config.pair)) {
+        request.btc = config.amount
+    } else if (Indodax.isPairIDR(config.pair)) {
+        request.idr = config.amount
+    } else {
+        return Promise.reject(ErrorProvider.ERRPAIR)
+    }
+    
+    return Indodax.execRequest(request).then(res => {
+        if (res.status === 200) {
+            if (res.data.success) {
+
+                return res.data.return
+            } else {
+
+                return Promise.reject(ErrorProvider.ERRTRADEBUY(res.data.return))
+            }
+        }
+    })
+}
+/**
+ * @description do sell trading
+ * @param config { Object }
+ * 
+ * @returns { Object }
+ */
+Action.prototype.sell = function (config) {
+    let pair = Indodax.toPair(config.pair)
+    let request = {
+        method: 'trade',
+        pair: pair,
+        type: 'sell',
+        price: config.price
+    }
+
+    if (Indodax.isPairBTC(config.pair)) {
+        request.btc = config.amount
+    } else if (Indodax.isPairIDR(config.pair)) {
+        request.idr = config.amount
+    } else {
+        return Promise.reject(ErrorProvider.ERRPAIR)
+    }
+
+    return Indodax.execRequest(request).then(res => {
+        if (res.status === 200) {
+            if (res.data.success) {
+
+                return res.data.return
+            } else {
+
+                return Promise.reject(ErrorProvider.ERRTRADEBUY(res.data.return))
+            }
+        }
+    })
 }
 
 Indodax.action = new Action()
